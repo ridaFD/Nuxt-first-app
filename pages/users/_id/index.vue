e634a5326f1dec04f3852a901c4fe3cab73013fa
Rida fd <ridafakherlden@gmail.com>
2021-02-04 02:23:02 +0200

    Creating a Route with a Dynamic Path

---
+++ pages/users/_id/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    profile page for the user {{ $route.params.id }}
+  </div>
+</template>
+
+<script>
+export default {
+name: "_show"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/users/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>users page</div>
+</template>
+
+<script>
+export default {
+name: "users"
+}
+</script>
+
+<style scoped>
+
+</style>
